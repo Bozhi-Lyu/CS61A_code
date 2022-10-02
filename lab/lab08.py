426a869272ff633ce132bf3a35dbd3eaf290e3a4
Bozhi-Lyu <2455523629@qq.com>
2022-10-02 21:50:01 +0800

    lab08 finished.

---
+++ lab/lab08.py
@@ -0,0 +1,46 @@
+def make_generators_generator(g):
+    """Generates all the "sub"-generators of the generator returned by
+    the generator function g.
+
+    >>> def every_m_ints_to(n, m):
+    ...     i = 0
+    ...     while (i <= n):
+    ...         yield i
+    ...         i += m
+    ...
+    >>> def every_3_ints_to_10():
+    ...     for item in every_m_ints_to(10, 3):
+    ...         yield item
+    ...
+    >>> for gen in make_generators_generator(every_3_ints_to_10):
+    ...     print("Next Generator:")
+    ...     for item in gen:
+    ...         print(item)
+    ...
+    Next Generator:
+    0
+    Next Generator:
+    0
+    3
+    Next Generator:
+    0
+    3
+    6
+    Next Generator:
+    0
+    3
+    6
+    9
+    """
+    
+    def gen_helper(nextnum):
+        thisgen = g() # a new generator
+        for _ in range(nextnum):
+            yield next(thisgen)
+    
+    nextnum = 1
+    for _ in range(len(list(g()))):
+        yield gen_helper(nextnum)
+        nextnum += 1
+
+
